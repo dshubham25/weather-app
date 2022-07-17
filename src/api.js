2299ee48c66a2103a3f332b05bce7803b1b48368
unknown <iamsds123@gmail.com>
2022-07-17 17:41:24 +0530

    make it better

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a8992e88d7msha8885d28201a61cp1e00d7jsna5bd00394c7b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "015733990aade2d407008c2baa7ace97";
